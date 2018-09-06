f53f85478d4578f8c94ab89d92efb481b3456cc7
unknown <rodolfojose@live.com>
2018-09-06 05:56:24 -0600

    tratando de logiarme a blackboards

---
+++ blackboarApp/blackboarApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace blackboarApp
+{
+	public partial class MainPage : ContentPage
+	{
+        public MainPage()
+        {
+            InitializeComponent();
+            
+		}
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                RestUser client = new RestUser();
+                var resulttoken = await client.Get<resultToken>("http://learn/api/public/v1/oauth2/authorizationcode");
+                if (resulttoken != null)
+                {
+                    LabelChange.Text = resulttoken.result.client_id;
+                }
+            });
+        }
+    }
+}
---
+++ blackboarApp/blackboarApp/RestUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace blackboarApp
+{
+    public class RestUser {
+        public async Task<T> Get<T>(string url)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                var response = await client.GetAsync(url);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var jsonstring = await response.Content.ReadAsStringAsync();
+                    return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(jsonstring);
+                }
+            }
+            catch
+            {
+
+            }
+            return default(T);
+        }
+}
+}
+
